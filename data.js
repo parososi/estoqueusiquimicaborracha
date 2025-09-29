a4fff5707c209580397020e00da462159d79a882
parososi <paulo.robertobr@tutamail.com>
2025-09-29 11:33:31 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,588 @@
+// Dados do Dashboard de Estoque v1.2 - Gerado em 29/09/2025, 11:32:57
+// Substitua este arquivo na pasta compartilhada para atualizar os dados
+console.log('Carregando data.js v1.2...');
+
+window.stockData = [
+  {
+    "code": "224.36.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 3421",
+    "stock14": 780,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0.7684729064039408,
+    "vendas4M": 4060
+  },
+  {
+    "code": "220.12.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 3431",
+    "stock14": 3980,
+    "stock9013": 8640,
+    "stock9015": 0,
+    "stockMonths": 1.386051619989017,
+    "vendas4M": 36420
+  },
+  {
+    "code": "225.36.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 3441",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 3160
+  },
+  {
+    "code": "219.12.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 3471",
+    "stock14": 3320,
+    "stock9013": 6900,
+    "stock9015": 0,
+    "stockMonths": 0.9775227164036346,
+    "vendas4M": 41820
+  },
+  {
+    "code": "229.36.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 3481",
+    "stock14": 2320,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 3.2447552447552446,
+    "vendas4M": 2860
+  },
+  {
+    "code": "226.36.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 5340",
+    "stock14": 899,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 1.3220588235294117,
+    "vendas4M": 2720
+  },
+  {
+    "code": "228.36.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 5370",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 11060
+  },
+  {
+    "code": "233.36.7",
+    "supplier": "HOSHINE",
+    "family": "BORRACHA SILICONE",
+    "item": "HOSHINE 5380",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 7800
+  },
+  {
+    "code": "252.35.2",
+    "supplier": "VINMAR",
+    "family": "BORRACHA EVA",
+    "item": "EVA SIPCHEM 2518",
+    "stock14": 345,
+    "stock9013": 18025,
+    "stock9015": 0,
+    "stockMonths": 6.1877894736842105,
+    "vendas4M": 11875
+  },
+  {
+    "code": "262.35.2",
+    "supplier": "VINMAR",
+    "family": "BORRACHA EVA",
+    "item": "EVA CYNPOL 0218",
+    "stock14": 7600,
+    "stock9013": 1200,
+    "stock9015": 36500,
+    "stockMonths": 1.8641975308641976,
+    "vendas4M": 97200
+  },
+  {
+    "code": "257.35.2",
+    "supplier": "KIUMHO",
+    "family": "BORRACHA EPDM",
+    "item": "EPDM KUMHO KEP 350",
+    "stock14": 6551,
+    "stock9013": 0,
+    "stock9015": 22650,
+    "stockMonths": 17.83539471675065,
+    "vendas4M": 6549
+  },
+  {
+    "code": "259.35.2",
+    "supplier": "KIUMHO",
+    "family": "BORRACHA EPDM",
+    "item": "EPDM KUMHO KEP 9570E",
+    "stock14": 4550,
+    "stock9013": 11900,
+    "stock9015": 0,
+    "stockMonths": 4.874074074074074,
+    "vendas4M": 13500
+  },
+  {
+    "code": "288.35.2",
+    "supplier": "KIUMHO",
+    "family": "BORRACHA EPDM",
+    "item": "KUMHO KEP 240",
+    "stock14": 350,
+    "stock9013": 0,
+    "stock9015": 500,
+    "stockMonths": 68,
+    "vendas4M": 50
+  },
+  {
+    "code": "290.35.2",
+    "supplier": "KIUMHO",
+    "family": "BORRACHA EPDM",
+    "item": "KUMHO KEP 570F",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 650
+  },
+  {
+    "code": "289.35.2",
+    "supplier": "KIUMHO",
+    "family": "BORRACHA EPDM",
+    "item": "KUMHO KEP 5770",
+    "stock14": 300,
+    "stock9013": 0,
+    "stock9015": 425,
+    "stockMonths": 116,
+    "vendas4M": 25
+  },
+  {
+    "code": "291.35.2",
+    "supplier": "KIUMHO",
+    "family": "BORRACHA EPDM",
+    "item": "KUMHO KEP 960NF",
+    "stock14": 250,
+    "stock9013": 0,
+    "stock9015": 300,
+    "stockMonths": 44,
+    "vendas4M": 50
+  },
+  {
+    "code": "256.35.2",
+    "supplier": "KUMHO",
+    "family": "EPM",
+    "item": "KUMHO 0520t",
+    "stock14": 0,
+    "stock9013": 2025,
+    "stock9015": 0,
+    "stockMonths": 1.35,
+    "vendas4M": 6000
+  },
+  {
+    "code": "260.35.2",
+    "supplier": "KUMHO",
+    "family": "EPM",
+    "item": "KUMHO 0530",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 11250
+  },
+  {
+    "code": "261.35.2",
+    "supplier": "KUMHO",
+    "family": "EPM",
+    "item": "KUMHO 0550",
+    "stock14": 0,
+    "stock9013": 10375,
+    "stock9015": 0,
+    "stockMonths": 1660,
+    "vendas4M": 25
+  },
+  {
+    "code": "274.35.2",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO EVA",
+    "item": "AZO 4-6",
+    "stock14": 0,
+    "stock9013": 1275,
+    "stock9015": 0,
+    "stockMonths": 1.5338345864661653,
+    "vendas4M": 3325
+  },
+  {
+    "code": "273.35.2",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO EVA",
+    "item": "AZO 6-8",
+    "stock14": 0,
+    "stock9013": 3075,
+    "stock9015": 0,
+    "stockMonths": 1.4260869565217391,
+    "vendas4M": 8625
+  },
+  {
+    "code": "266.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "CBS",
+    "stock14": 1225,
+    "stock9013": 2825,
+    "stock9015": 0,
+    "stockMonths": 2.012422360248447,
+    "vendas4M": 8050
+  },
+  {
+    "code": "265.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "MBTS",
+    "stock14": 3300,
+    "stock9013": 8075,
+    "stock9015": 0,
+    "stockMonths": 55.15151515151515,
+    "vendas4M": 825
+  },
+  {
+    "code": "264.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "TMTD",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 7275
+  },
+  {
+    "code": "272.35.2",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "TMQ",
+    "stock14": 450,
+    "stock9013": 4400,
+    "stock9015": 0,
+    "stockMonths": 5.464788732394366,
+    "vendas4M": 3550
+  },
+  {
+    "code": "270.35.7",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "6PPD",
+    "stock14": 550,
+    "stock9013": 775,
+    "stock9015": 0,
+    "stockMonths": 0.527363184079602,
+    "vendas4M": 10050
+  },
+  {
+    "code": "271.35.7",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "PVI",
+    "stock14": 900,
+    "stock9013": 200,
+    "stock9015": 0,
+    "stockMonths": 2.6666666666666665,
+    "vendas4M": 1650
+  },
+  {
+    "code": "269.35.2",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "RESINA C9",
+    "stock14": 1075,
+    "stock9013": 8200,
+    "stock9015": 0,
+    "stockMonths": 371,
+    "vendas4M": 100
+  },
+  {
+    "code": "268.35.2",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "PEG 4000",
+    "stock14": 825,
+    "stock9013": 2400,
+    "stock9015": 0,
+    "stockMonths": 43,
+    "vendas4M": 300
+  },
+  {
+    "code": "267.35.7",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "SI69 SILANO",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 1150
+  },
+  {
+    "code": "276.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "DTDM",
+    "stock14": 550,
+    "stock9013": 3000,
+    "stock9015": 0,
+    "stockMonths": 284,
+    "vendas4M": 50
+  },
+  {
+    "code": "277.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "TMTM",
+    "stock14": 500,
+    "stock9013": 1200,
+    "stock9015": 0,
+    "stockMonths": 272,
+    "vendas4M": 25
+  },
+  {
+    "code": "278.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "DPTT",
+    "stock14": 650,
+    "stock9013": 2950,
+    "stock9015": 0,
+    "stockMonths": 576,
+    "vendas4M": 25
+  },
+  {
+    "code": "279.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "ZDEC",
+    "stock14": 550,
+    "stock9013": 1200,
+    "stock9015": 0,
+    "stockMonths": 140,
+    "vendas4M": 50
+  },
+  {
+    "code": "280.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "ZDMC",
+    "stock14": 825,
+    "stock9013": 550,
+    "stock9015": 0,
+    "stockMonths": 12.941176470588236,
+    "vendas4M": 425
+  },
+  {
+    "code": "281.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "ZDBC",
+    "stock14": 1125,
+    "stock9013": 2400,
+    "stock9015": 0,
+    "stockMonths": 188,
+    "vendas4M": 75
+  },
+  {
+    "code": "282.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "MOZ/NOBZ",
+    "stock14": 550,
+    "stock9013": 600,
+    "stock9015": 0,
+    "stockMonths": 184,
+    "vendas4M": 25
+  },
+  {
+    "code": "283.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "MBT",
+    "stock14": 1100,
+    "stock9013": 1200,
+    "stock9015": 0,
+    "stockMonths": 92,
+    "vendas4M": 100
+  },
+  {
+    "code": "287.35.2",
+    "supplier": "ALLMARI",
+    "family": "CARGAS BORRACHA",
+    "item": "SILICA",
+    "stock14": 3380,
+    "stock9013": 15060,
+    "stock9015": 0,
+    "stockMonths": 2.890282131661442,
+    "vendas4M": 25520
+  },
+  {
+    "code": "284.35.7",
+    "supplier": "ALLMARI",
+    "family": "ADITIVO BORRACHA",
+    "item": "RESINA HM1300",
+    "stock14": 4025,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0.7853658536585366,
+    "vendas4M": 20500
+  },
+  {
+    "code": "308.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "CBS MASTER",
+    "stock14": 555,
+    "stock9013": 1800,
+    "stock9015": 0,
+    "stockMonths": 376.8,
+    "vendas4M": 25
+  },
+  {
+    "code": "309.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "TMTD MASTER",
+    "stock14": 575,
+    "stock9013": 900,
+    "stock9015": 0,
+    "stockMonths": 236,
+    "vendas4M": 25
+  },
+  {
+    "code": "302.35.2",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "MBTS MASTER",
+    "stock14": 530,
+    "stock9013": 2400,
+    "stock9015": 0,
+    "stockMonths": 468.8,
+    "vendas4M": 25
+  },
+  {
+    "code": "303.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "DPG",
+    "stock14": 575,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 92,
+    "vendas4M": 25
+  },
+  {
+    "code": "305.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "TBBS",
+    "stock14": 600,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 96,
+    "vendas4M": 25
+  },
+  {
+    "code": "304.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "NA22",
+    "stock14": 0,
+    "stock9013": 600,
+    "stock9015": 0,
+    "stockMonths": 96,
+    "vendas4M": 25
+  },
+  {
+    "code": "306.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "ZMTI",
+    "stock14": 475,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 76,
+    "vendas4M": 25
+  },
+  {
+    "code": "307.35.7",
+    "supplier": "ALLMARI",
+    "family": "ACELERADOR",
+    "item": "TETD",
+    "stock14": 0,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 0,
+    "vendas4M": 600
+  },
+  {
+    "code": "293.60.2",
+    "supplier": "ALLMARI",
+    "family": "NEGRO DE FUMO",
+    "item": "NF 550 OMSK",
+    "stock14": 0,
+    "stock9013": 37800,
+    "stock9015": 0,
+    "stockMonths": 6048,
+    "vendas4M": 25
+  },
+  {
+    "code": "292.60.2",
+    "supplier": "ALLMARI",
+    "family": "NEGRO DE FUMO",
+    "item": "NF 339 OMSK",
+    "stock14": 0,
+    "stock9013": 18700,
+    "stock9015": 0,
+    "stockMonths": 2992,
+    "vendas4M": 25
+  },
+  {
+    "code": "237.12.3",
+    "supplier": "ALLMARI",
+    "family": "FLUIDO",
+    "item": "USIPMX 1000 (19kg)",
+    "stock14": 57,
+    "stock9013": 0,
+    "stock9015": 0,
+    "stockMonths": 4,
+    "vendas4M": 57
+  },
+  {
+    "code": "237.39.2",
+    "supplier": "ALLMARI",
+    "family": "FLUIDO",
+    "item": "USIPMX 1000 (190kg)",
+    "stock14": 437,
+    "stock9013": 760,
+    "stock9015": 0,
+    "stockMonths": 191.52,
+    "vendas4M": 25
+  }
+];
+
+// Metadados
+window.stockMetadata = {
+    lastUpdate: "2025-09-29T14:32:57.922Z",
+    totalProducts: 52,
+    generatedBy: "Dashboard de Estoque v1.2 - Mapeamento Corrigido",
+    version: "1.2.0"
+};
+
+console.log('data.js v1.2 carregado com', window.stockData.length, 'produtos');
